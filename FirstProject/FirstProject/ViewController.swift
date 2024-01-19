f5e5fcc7437765f7c21bb99dee839c4bce904a81
s1thu <s1thu.w1n.5601@gmail.com>
2024-01-19 21:34:25 +0630

    adding projects

---
+++ FirstProject/FirstProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstProject
+//
+//  Created by Sithu Win on 15/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Sithu Win on 06/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
