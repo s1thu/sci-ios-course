cb184edc284acdde9bc9fee1d86be199476fc946
s1thu <s1thu.w1n.5601@gmail.com>
2024-01-24 19:22:17 +0630

    Adaptivestackview and ui final day

---
+++ adaptivestackview/adaptivestackview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  adaptivestackview
+//
+//  Created by Sithu Win on 21/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
