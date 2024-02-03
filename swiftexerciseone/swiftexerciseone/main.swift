7e4f9de3f0ad957e20c1b683559ac4455dd31e63
s1thu <s1thu.w1n.5601@gmail.com>
2024-02-03 21:49:54 +0630

    looping

--- swiftexerciseone/swiftexerciseone/main.swift
+++ swiftexerciseone/swiftexerciseone/main.swift
@@ -21,3 +21,29 @@ print("Please enter number 2: ", terminator: " ")
 let numberTwo = Int( readLine() ?? "0") ?? 0
 
 print("Result of \(numberOne) + \(numberTwo) = \(numberOne+numberTwo)")
+
+for i in 0...10 where i == 0{
+    print(i)
+}
+
+for char in "Hello" where char == "H" {
+    print(char)
+}
+
+//for i in 0...10{
+//    for j in 0...20{
+//        for k in 0...5{
+//            print("\(i) \(j) \(k)")
+//        }
+//        
+//    }
+//}
+
+for i in 0...7{
+    print("Start of \(i) Multiplication Table")
+    for j in 0...7{
+        print("\(i) x \(j) = \(i*j)")
+    }
+    print("End of  \(i) Multiplication Table")
+    print()
+}
