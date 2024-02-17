4de3283ebb58d72097742ad25d8edbcf0263c641
s1thu <s1thu.w1n.5601@gmail.com>
2024-02-17 18:52:44 +0630

    Function

--- swiftfoundation.playground/Contents.swift
+++ swiftfoundation.playground/Contents.swift
@@ -1,4 +1,5 @@
 import UIKit
+import Foundation
 
 var greeting = "Hello, playground"
 
@@ -89,3 +90,155 @@ print(reversedStr)
 if(testStr == reversedStr){
     "\(testStr) is palindrone"
 }
+
+
+func sayHelloWorld(){
+    print("hello")
+    print("world")
+}
+
+func generateStar(){
+    for i in 1...5{
+        for j in 1...i{
+            print("* ",terminator: " ")
+        }
+        print()
+    }
+    
+}
+
+sayHelloWorld()
+generateStar()
+
+print("Hello World")
+print("Hello")
+
+func add(of n1:Int, and n2:Int){
+    print(n1+n2)
+}
+
+func substract(of n1:Int, and n2:Int){
+    print(n1-n2)
+}
+func multiply(of n1:Int, and n2:Int){
+    print(n1*n2)
+}
+func divided(of n1:Int, and n2:Int = 5){
+    print(n1/n2)
+}
+
+add(of: 1, and: 2)
+substract(of: 10, and: 9)
+multiply(of: 255, and: 255)
+divided(of: 10)
+
+func variadicParamTest(nums: Int...){
+    var result = 0
+    for num in nums{
+        if result == 0 {
+            result = num
+        }
+        else{
+            result *= num;
+        }
+        
+    }
+    print(result)
+}
+
+variadicParamTest(nums: 1,2,3,4,5,6,7,8,9,10)
+
+//Pass by reference function
+var a = 10
+var b = 20
+func swapValue(swapOne: inout Int, swapTwo: inout Int){
+    let tmp = swapOne
+    swapOne = swapTwo
+    swapTwo = tmp
+}
+
+swapValue(swapOne: &a, swapTwo: &b)
+print("\(a) \(b)")
+
+func validateUser(of userName: String, and password: String) {
+    
+    //clousure -> lambda
+    func validateUserName(userName: String) {
+        
+        if userName.isEmpty {
+            
+            print("User name must not be empty")
+            
+        }
+        
+    }
+    
+    func validatePassword(password: String) {
+        
+        if password.isEmpty {
+            
+            print("Password must not be emoty")
+            
+        }
+        
+    }
+    
+    validateUserName(userName: userName)
+    
+    validatePassword(password: password)
+}
+
+validateUser(of: "", and: "pass123")
+
+func returnFunc(output: String)-> String{
+    return output
+}
+
+func returnNum(n1:Int,n2:Int) -> Int{
+    n1+n2
+}
+
+print(returnFunc(output: "Hello"))
+
+//higher order function or functional programming
+func mathOperation(n1: Int, n2:Int, mathFunc:(Int ,Int)-> Int) -> Int{
+    mathFunc(n1,n2)
+}
+
+let addFuncVar = returnNum
+
+let resultOne = mathOperation(n1: 1, n2: 2, mathFunc: addFuncVar)
+print(resultOne)
+
+
+mathOperation(n1: 1, n2: 1) { $0 + $1}
+let resultTwo:Int = mathOperation(n1: 5, n2: 10) { n1, n2 in
+    n1-n2
+}
+
+print(resultTwo)
+
+2.isEven()
+
+extension Int{
+    func isEven() -> Bool{
+        self % 2 == 0
+    }
+}
+
+extension Int{
+    func factorial() -> Int{
+        var result:Int = 0
+        for i in 1...self{
+            if(result == 0){
+                result = i
+            }else{
+                result *= i
+            }
+        }
+        return result
+    }
+}
+
+print(10.factorial())
+print(1.factorial())
