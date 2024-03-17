5d81864ad75c04bd16a8d95ac3710a91c0b61f3f
s1thu <s1thu.w1n.5601@gmail.com>
2024-03-17 22:44:53 +0630

    UITableView -> Cell,Datasource,Delegate,DataModel

---
+++ uitblView/uitblView/BookCell.swift
@@ -0,0 +1,41 @@
+//
+//  BookCell.swift
+//  uitblView
+//
+//  Created by Sithu Win on 17/03/2024.
+//
+
+import UIKit
+
+class BookCell: UITableViewCell {
+
+//    @IBOutlet weak var lblBookTitle:UILabel!
+    @IBOutlet weak var lblBookTitle:UILabel!
+    @IBOutlet weak var lblBookPrice:UILabel!
+    
+    var data:Book? = nil{
+        didSet{
+            if let data = data{
+                lblBookTitle.text = data.bookName
+            }
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        selectionStyle = .none
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func bindData(data:Book){
+        lblBookTitle.text = data.bookName
+        
+    }
+    
+}
