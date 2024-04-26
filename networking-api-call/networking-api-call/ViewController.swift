6a07ce79bef80f6be9160291d485d5164335e379
s1thu <s1thu.w1n.5601@gmail.com>
2024-04-26 22:16:23 +0630

    Read API from https

--- networking-api-call/networking-api-call/ViewController.swift
+++ networking-api-call/networking-api-call/ViewController.swift
@@ -8,11 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private var characters:[CharacterVO] = []{
+        didSet
+        {
+            for item in characters{
+                print("\(item.id) \(item.name)")
+            }
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        fetchDataFromJsonFile()
+//        fetchDataFromJsonFile()
+        fetchDataFromURL()
     }
 
     func fetchDataFromJsonFile(){
@@ -34,6 +44,52 @@ class ViewController: UIViewController {
                 print(error.localizedDescription)
             }
         }
+    }
+    
+    func fetchDataFromURL(){
+        //Step 1 Get URL
+        let url:URL? = URL(string: "https://rickandmortyapi.com/api/character")
+        
+        //Step 2 Request => HTTP method
+        guard let url = url else { return  }
+        var request = URLRequest.init(url: url)
+        
+        //Step 3 URL Request => HTTPmethod
+        request.httpMethod = "GET"
+        
+        //Step 4 Set Content Type
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        //Initialize URLSession
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error{
+                print(error.localizedDescription)
+            }else{
+                
+//                //Step 6 HTTP Status Code Check
+//                if let statuscode = (response as? HTTPURLResponse)?.statusCode{
+//                    //Step 7 Status code is between 200 < 300 ?
+//                    if(200..<300) ~= statuscode{
+//                        
+//                    }
+//                }
+                
+                //Step 7 Decode
+                if let data = data{
+                    do{
+                        
+                        let data:RickMortyResponse = try JSONDecoder().decode(RickMortyResponse.self, from: data)
+                        
+                        DispatchQueue.main.async {
+                            self.characters = data.results
+                        }
+                        
+                    }catch{
+                        print(error.localizedDescription)
+                    }
+                }
+            }
+        }.resume()
     }
 
 }
